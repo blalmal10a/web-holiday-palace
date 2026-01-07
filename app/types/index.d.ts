2005994bc5e9da564a3d95cc9157030fae284220
blalmal10a <malsawma7777@gmail.com>
2026-01-07 08:37:36 +0000

    chore: add type definition for user and pagination

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,5 +1,5 @@
 interface User {
-    id: number;
+    id: string;
     name: string;
     email: string;
     email_verified_at: string | null | undefined;
@@ -18,3 +18,27 @@ interface Role {
     }
 }
 
+interface Pagination {
+    current_page: number;
+    data: any[];
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: {
+        url: string | null;
+        label: string;
+        page: number | null;
+        active: boolean;
+    }
+    next_page_url: string | null;
+    path: string;
+    per_page: number;
+    prev_page_url: string | null;
+    to: number;
+    total: number;
+}
+
+interface PaginationUser extends Pagination {
+    data: User[]
+}
