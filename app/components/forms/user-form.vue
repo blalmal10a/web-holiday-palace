978dcf46d5b66343e4717872fca2c6545e4da19f
blalmal10a <malsawma7777@gmail.com>
2026-01-07 10:23:39 +0000

    feat: add user form

---
+++ app/components/forms/user-form.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup>
+	useHead({
+		title: "Settings",
+	})
+	onMounted(() => {
+		//
+	})
+
+	const toast = useToast()
+	async function onSubmit() {
+		await auth.onSubmitUpdateProfile()
+		toast.add({
+			title: "Profile updated successfully",
+		})
+	}
+	const userStore = useUserStore()
+</script>
+<template>
+	<div
+		class="flex flex-col items-center"
+		style="height: calc(100dvh - 200px); overflow: visible"
+	>
+		<u-card style="min-width: min(400px, 90vw)">
+			<u-form
+				:schema="userFormSchema"
+				:state="userStore.form"
+				@submit="userStore.submitUserForm()"
+				class="space-y-4 w-full"
+			>
+				<u-form-field label="name" name="name">
+					<u-input
+						v-model="userStore.form.name"
+						icon="i-lucide-user"
+						type="text"
+					/>
+				</u-form-field>
+				<u-form-field label="email" name="email">
+					<u-input
+						v-model="userStore.form.email"
+						icon="i-lucide-mail"
+						type="email"
+					/>
+				</u-form-field>
+				<u-form-field label="password" name="password">
+					<u-input
+						v-model="userStore.form.password"
+						icon="i-lucide-lock"
+						type="password"
+						autocomplete="new-password"
+					/>
+				</u-form-field>
+				<u-form-field label="confirm password" name="password_confirmation">
+					<u-input
+						v-model="userStore.form.password_confirmation"
+						icon="i-lucide-lock"
+						type="password"
+						autocomplete="new-password"
+					/>
+				</u-form-field>
+				<div class="text-right">
+					<u-button
+						:loading="auth.loadingSubmitUpdateProfile"
+						class=""
+						type="submit"
+					>
+						Submit
+					</u-button>
+				</div>
+			</u-form>
+		</u-card>
+	</div>
+</template>
+
+<style scoped>
+	.relative.inline-flex.items-center {
+		width: 100%;
+	}
+</style>
---
+++ app/pages/hotels/users/[id]/form.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+	definePageMeta({
+		layout: "main-layout",
+		pageTitle: "Users",
+		ancestors: [],
+	})
+	const userStore = useUserStore()
+	onMounted(() => {
+		if (!userStore.form.id && useRoute().params.id != "add") {
+			userStore.getDetail()
+		}
+	})
+</script>
+<template>
+	<div>
+		<FormsUserForm />
+	</div>
+</template>
+
+<style></style>
