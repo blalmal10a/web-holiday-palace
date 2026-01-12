182cce2168dfa4bde235eebbbe6a5610cd56dff7
blalmal10a <malsawma7777@gmail.com>
2026-01-12 07:53:43 +0000

    refactor: update UI

--- app/components/bookings/calendar.vue
+++ app/components/bookings/calendar.vue
@@ -1,125 +1,128 @@
 <script setup lang="ts">
-import { addDays, format } from 'date-fns';
+	import {addDays, format} from "date-fns"
 
-const roomStore = useRoomStore()
-const room = useRoom();
-const booking = useBooking();
-const bookingStore = useBookingStore();
-const calendarStore = useCalendarStore();
-await room.fetchData();
-await booking.fetchData();
-calendarStore.$reset();
-calendarStore.initCalendar(roomStore.data.data, bookingStore.data.data)
-function shouldRender(cellIndex: number, cell: CalendarData) {
-    let shouldRender = false;
+	const roomStore = useRoomStore()
+	const room = useRoom()
+	const booking = useBooking()
+	const bookingStore = useBookingStore()
+	const calendarStore = useCalendarStore()
+	await room.fetchData()
+	await booking.fetchData()
+	calendarStore.$reset()
+	calendarStore.initCalendar(roomStore.data.data, bookingStore.data.data)
+	function shouldRender(cellIndex: number, cell: CalendarData) {
+		let shouldRender = false
 
-    if (cell.bookingInfo && cell.start_cell) {
-        shouldRender = true;
-    }
-    if (!cell.bookingInfo) {
-        shouldRender = true;
-    }
-    if (cellIndex == 0) {
-        shouldRender = false;
-    }
-    return shouldRender;
-}
-
+		if (cell.bookingInfo && cell.start_cell) {
+			shouldRender = true
+		}
+		if (!cell.bookingInfo) {
+			shouldRender = true
+		}
+		if (cellIndex == 0) {
+			shouldRender = false
+		}
+		return shouldRender
+	}
 </script>
 <template>
-    <div>
-        <div class="w-full overflow-auto pt-4">
-            <table>
-                <thead>
-                    <tr>
-                        <th class=" bg-default ">
-                            <div class=" h-24 flex items-center justify-center border border-default">
-                                HP
-                            </div>
+	<div>
+		<div class="w-full overflow-auto pt-4">
+			<table>
+				<thead>
+					<tr>
+						<th class="bg-default">
+							<div
+								class="h-24 flex items-center justify-center border border-default"
+							>
+								HP
+							</div>
+						</th>
+						<template v-for="date in calendarStore.dateList">
+							<th
+								class="text-center p-4 border-default border text-sm whitespace-nowrap"
+							>
+								{{ format(date, "Do MMM") }}
+								<div>({{ format(date, "EEE") }})</div>
+							</th>
+						</template>
+					</tr>
+				</thead>
+				<template v-for="(row, rowIndex) in calendarStore.data" :key="rowIndex">
+					<tr>
+						<template v-for="(cell, cellIndex) in row" :key="cellIndex">
+							<td v-if="cellIndex == 0" class="bg-default">
+								<div class="py-4 px-1 border border-default">
+									<div
+										class="py-4 px-1 h-8 text-xs flex items-center max-w-24"
+										style="text-overflow: ellipsis"
+									>
+										<div class="w-full truncate">
+											{{ cell.room.name }}
+										</div>
+										<span
+											class="pointer-events-none absolute -top-8 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100 bg-black text-white p-1 rounded"
+										>
+											{{ cell.room.name }}
+										</span>
+									</div>
+								</div>
+							</td>
 
-                        </th>
-                        <template v-for="date in calendarStore.dateList">
-                            <th class="text-center p-4 border-default border text-sm whitespace-nowrap">
-                                {{ format(date, 'Do MMM') }}
-                                <div>
-                                    ({{ format(date, 'EEE') }})
-                                </div>
-                            </th>
-                        </template>
-                    </tr>
-                </thead>
-                <template
-                    v-for="(row, rowIndex) in calendarStore.data"
-                    :key="rowIndex"
-                >
-                    <tr>
-
-                        <template
-                            v-for="(cell, cellIndex) in row"
-                            :key="cellIndex"
-                        >
-                            <td
-                                v-if="cellIndex == 0"
-                                class="bg-default"
-                            >
-                                <div class="py-4 px-1 border border-default">
-                                    <div class="py-4 px-1 whitespace-nowrap  h-10 text-xs">
-                                        {{ cell.room.name }}
-                                    </div>
-                                </div>
-                            </td>
-
-                            <td
-                                v-if="shouldRender(cellIndex, cell)"
-                                :colspan="cell.cellLength"
-                                @click="() => {
-                                    if (!cell.bookingInfo) {
-                                        if (cell.bookingInfo) {
-                                            bookingStore.setForm(cell.bookingInfo)
-                                        } else {
-                                            bookingStore.form.room_id = cell.room.id;
-                                            bookingStore.form.check_in_date = cell.date;
-                                            bookingStore.form.checkout_date = format(addDays(cell.date, 1), 'yyyy-MM-dd');
-                                        }
-                                        useRouter().push({
-                                            hash: '#booking',
-                                        })
-                                    }
-                                }"
-                            >
-                                <BookingsCalendarMenu :cell="cell">
-                                    <div class="p-4 border border-default">
-                                        <div
-                                            class="  rounded-lg  px-2 whitespace-nowrap  flex items-center h-8 relative"
-                                            :class="{
-                                                'text-primary bg-elevated': !!cell.bookingInfo
-                                            }"
-                                        >
-                                            {{ cell.bookingInfo?.client?.name }}
-
-
-                                        </div>
-                                    </div>
-                                </BookingsCalendarMenu>
-                            </td>
-                        </template>
-                    </tr>
-                </template>
-            </table>
-        </div>
-        <BookingsFullScreenModal />
-        <InvoicesFullScreenModal />
-    </div>
+							<td
+								v-if="shouldRender(cellIndex, cell)"
+								:colspan="cell.cellLength"
+								@click="
+									() => {
+										if (!cell.bookingInfo) {
+											if (cell.bookingInfo) {
+												bookingStore.setForm(cell.bookingInfo)
+											} else {
+												bookingStore.form.room_id = cell.room.id
+												bookingStore.form.check_in_date = cell.date
+												bookingStore.form.checkout_date = format(
+													addDays(cell.date, 1),
+													'yyyy-MM-dd',
+												)
+											}
+											useRouter().push({
+												hash: '#booking',
+											})
+										}
+									}
+								"
+							>
+								<BookingsCalendarMenu :cell="cell">
+									<div class="p-4 border border-default">
+										<div
+											class="rounded-lg px-2 whitespace-nowrap flex items-center h-8 relative"
+											:class="{
+												'text-primary bg-elevated': !!cell.bookingInfo,
+											}"
+										>
+											{{ cell.bookingInfo?.client?.name }}
+										</div>
+									</div>
+								</BookingsCalendarMenu>
+							</td>
+						</template>
+					</tr>
+				</template>
+			</table>
+		</div>
+		<BookingsFullScreenModal />
+		<InvoicesFullScreenModal />
+	</div>
 </template>
 <style scoped>
-table {
-    border-collapse: collapse;
-}
+	table {
+		border-collapse: collapse;
+	}
 
-th:first-child,
-td:first-child {
-    position: sticky;
-    left: 0;
-    z-index: 10;
-}
+	th:first-child,
+	td:first-child {
+		position: sticky;
+		left: 0;
+		z-index: 10;
+	}
 </style>
