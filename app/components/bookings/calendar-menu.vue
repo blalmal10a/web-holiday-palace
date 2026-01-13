04d9b4937e1a2da414b9c0392d7e204231e2abe3
blalmal10a <malsawma7777@gmail.com>
2026-01-14 03:07:10 +0530

    faet: handle main data change

--- app/components/bookings/calendar-menu.vue
+++ app/components/bookings/calendar-menu.vue
@@ -1,82 +1,82 @@
 <script setup lang="ts">
-	import type {DropdownMenuItem} from "@nuxt/ui"
-	import {addDays, format} from "date-fns"
-	const props = defineProps({
-		cell: {
-			default: {} as CalendarData,
-			required: true,
+import type { DropdownMenuItem } from "@nuxt/ui"
+import { addDays, format } from "date-fns"
+const props = defineProps({
+	cell: {
+		default: {} as CalendarData,
+		required: true,
+	},
+})
+const bookingStore = useBookingStore()
+const calendarStore = useCalendarStore()
+const items: DropdownMenuItem[] = [
+	{
+		label: "Booking",
+		icon: "i-lucide-calendar",
+		onClick: () => {
+			if (props.cell.bookingInfo) {
+				bookingStore.setForm({ ...props.cell.bookingInfo })
+			} else {
+				bookingStore.form.room_id = props.cell.room.id
+				bookingStore.form.check_in_date = props.cell.date
+				bookingStore.form.checkout_date = format(
+					addDays(props.cell.date, 1),
+					"yyyy-MM-dd",
+				)
+			}
+			useRouter().push({
+				hash: "#booking",
+			})
 		},
-	})
-	const bookingStore = useBookingStore()
-	const calendarStore = useCalendarStore()
-	const items: DropdownMenuItem[] = [
-		{
-			label: "Booking",
-			icon: "i-lucide-calendar",
-			onClick: () => {
-				if (props.cell.bookingInfo) {
-					bookingStore.setForm(props.cell.bookingInfo)
-				} else {
-					bookingStore.form.room_id = props.cell.room.id
-					bookingStore.form.check_in_date = props.cell.date
-					bookingStore.form.checkout_date = format(
-						addDays(props.cell.date, 1),
-						"yyyy-MM-dd",
-					)
-				}
-				useRouter().push({
-					hash: "#booking",
+	},
+	{
+		label: "Invoice",
+		icon: "i-lucide-receipt-text",
+		disabled: !props.cell.bookingInfo?.invoice?.id,
+		onClick: () => {
+			if (!props.cell.bookingInfo?.invoice?.id) {
+				useToast().add({
+					title: "Invoice not found",
+					description: "Invoice not found",
+					color: "error",
 				})
-			},
+				return
+			}
+			useInvoiceStore().form = props.cell.bookingInfo?.invoice
+			useRouter().push({
+				name: "hotels-calendar",
+				hash: "#invoice",
+			})
 		},
-		{
-			label: "Invoice",
-			icon: "i-lucide-receipt-text",
-			disabled: !props.cell.bookingInfo?.invoice?.id,
-			onClick: () => {
-				if (!props.cell.bookingInfo?.invoice?.id) {
-					useToast().add({
-						title: "Invoice not found",
-						description: "Invoice not found",
-						color: "error",
-					})
-					return
-				}
-				useInvoiceStore().form = props.cell.bookingInfo?.invoice
-				useRouter().push({
-					name: "hotels-calendar",
-					hash: "#invoice",
-				})
-			},
+	},
+	// {
+	//     label: 'Payments',
+	//     icon: 'i-lucide-hand-coins',
+	//     disabled: !props.cell.bookingInfo?.invoice?.id,
+	// },
+	{
+		label: "Print invoice",
+		icon: "i-lucide-printer",
+		disabled: !props.cell.bookingInfo?.invoice?.id,
+		onClick: () => {
+			useRouter().push({
+				name: "hotels-invoices-id",
+				params: {
+					id: props.cell.bookingInfo?.id,
+				},
+			})
 		},
-		// {
-		//     label: 'Payments',
-		//     icon: 'i-lucide-hand-coins',
-		//     disabled: !props.cell.bookingInfo?.invoice?.id,
-		// },
-		{
-			label: "Print invoice",
-			icon: "i-lucide-printer",
-			disabled: !props.cell.bookingInfo?.invoice?.id,
-			onClick: () => {
-				useRouter().push({
-					name: "hotels-invoices-id",
-					params: {
-						id: props.cell.bookingInfo?.id,
-					},
-				})
-			},
-		},
-	]
+	},
+]
 
-	// watch router hash
+// watch router hash
 </script>
 
 <template>
 	<UDropdownMenu
 		v-if="props.cell.bookingInfo"
 		:items="items"
-		:ui="{content: 'w-48'}"
+		:ui="{ content: 'w-48' }"
 	>
 		<slot />
 	</UDropdownMenu>
