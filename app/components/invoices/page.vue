161a35f4c03b29bd182625551b40dec7f7c9a28a
blalmal10a <malsawma7777@gmail.com>
2026-01-21 09:52:55 +0000

    chore: install html 2 canvas, implement canvas display

--- app/components/invoices/page.vue
+++ app/components/invoices/page.vue
@@ -11,84 +11,92 @@
 	const invoice = ref<Invoice>(bookingStore.detail.invoice)
 </script>
 <template>
-	<div class="min-h-dvh font-new-roman">
-		<div
-			class="border border-gray-200 rounded-xl invoice-container p-8 bg-white text-black"
-			style=""
-		>
-			<div class="text-center font-semibold text-lg">BILL</div>
-			<h1
-				class="text-3xl text-center"
-				style="font-family: 'Arial'; font-weight: bolder"
-			>
-				HOLIDAY PALACE
-			</h1>
-			<div
-				class="text-center relative"
-				style="font-family: 'Times New Roman', Times, serif"
-			>
-				<div class="left-0 absolute text-lg" style="top: 11pt">
-					GSTIN: 15DSGPS2599K1ZZ
+	<div
+		class="min-h-dvh font-new-roman"
+		style="position: fixed; top: 100vh; right: 100vw"
+	>
+		<div class="border border-gray-200 rounded-xl bg-white text-black">
+			<div style="" class="invoice-container p-8" id="invoice-a4">
+				<div class="text-center font-semibold text-lg">BILL</div>
+				<h1
+					class="text-3xl text-center"
+					style="font-family: Arial; font-weight: bolder"
+				>
+					HOLIDAY PALACE
+				</h1>
+				<div
+					class="text-center relative"
+					style="
+						font-family:
+							Times New Roman,
+							Times,
+							serif;
+					"
+				>
+					<div class="left-0 absolute text-lg" style="top: 11pt">
+						GSTIN: 15DSGPS2599K1ZZ
+					</div>
+					<h3 class="text-xl font-semibold">
+						<div>Sihhmui, Sairang</div>
+						<div>Regn. No. MSE/281/2012</div>
+						<div>Ph. No. 9436154813/8837382366</div>
+					</h3>
 				</div>
-				<h3 class="text-xl font-semibold">
-					<div>Sihhmui, Sairang</div>
-					<div>Regn. No. MSE/281/2012</div>
-					<div>Ph. No. 9436154813/8837382366</div>
-				</h3>
-			</div>
-			<div class="flex justify-between">
-				<div>No.: {{ bookingStore.detail.invoice.invoice_no }}</div>
-				<div>Date: {{ format(new Date(), "d/MM/yyyy") }}</div>
-			</div>
-			<div class="flex justify-center">
-				<div class="w-96 flex">
-					<div>To,</div>
-					<div class="flex-1 pl-2">
-						<div class="border-b">{{ bookingStore.detail.client.name }}</div>
-						<div class="border-b mt-2">
-							{{ format(bookingStore.detail.check_in_date, "Do MMM, yyyy") }} to
-							{{ format(bookingStore.detail.checkout_date, "Do MMM, yyyy") }}
+				<div class="flex justify-between">
+					<div>No.: {{ bookingStore.detail.invoice.invoice_no }}</div>
+					<div>Date: {{ format(new Date(), "d/MM/yyyy") }}</div>
+				</div>
+				<div class="flex justify-center">
+					<div class="w-96 flex">
+						<div>To,</div>
+						<div class="flex-1 pl-2">
+							<div class="border-b">{{ bookingStore.detail.client.name }}</div>
+							<div class="border-b mt-2">
+								{{ format(bookingStore.detail.check_in_date, "Do MMM, yyyy") }}
+								to
+								{{ format(bookingStore.detail.checkout_date, "Do MMM, yyyy") }}
+							</div>
 						</div>
 					</div>
 				</div>
+				<table class="mt-2 w-full">
+					<thead>
+						<tr>
+							<th style="width: 18mm">Sl. No.</th>
+							<th colspan="3">Description</th>
+							<th style="width: 20mm">Rate.</th>
+							<th style="width: 18mm">Qty.</th>
+							<th style="width: 25mm">Amount</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="(item, index) in bookingStore.detail.invoice.items">
+							<td>{{ index + 1 }}</td>
+							<td colspan="3">
+								{{ item.description }}
+							</td>
+							<td>{{ item.rate }}</td>
+							<td>{{ item.quantity }}</td>
+							<td>{{ item.rate * item.quantity }}</td>
+						</tr>
+						<tr v-for="value in minRows">
+							<td></td>
+							<td colspan="3"></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr class="font-bold">
+							<td style="border-left: none" colspan="6" class="text-right">
+								TOTAL
+							</td>
+							<td style="border-bottom: 1px solid black">
+								{{ invoice?.total_amount }}
+							</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
-			<table class="mt-2 w-full">
-				<thead>
-					<tr>
-						<th style="width: 18mm">Sl. No.</th>
-						<th colspan="3">Description</th>
-						<th style="width: 20mm">Rate.</th>
-						<th style="width: 18mm">Qty.</th>
-						<th style="width: 25mm">Amount</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="(item, index) in bookingStore.detail.invoice.items">
-						<td>{{ index + 1 }}</td>
-						<td colspan="3">
-							{{ item.description }}
-						</td>
-						<td>{{ item.rate }}</td>
-						<td>{{ item.quantity }}</td>
-						<td>{{ item.rate * item.quantity }}</td>
-					</tr>
-					<tr v-for="value in minRows">
-						<td></td>
-						<td colspan="3"></td>
-						<td></td>
-						<td></td>
-						<td></td>
-					</tr>
-					<tr class="font-bold">
-						<td style="border-left: none" colspan="6" class="text-right">
-							TOTAL
-						</td>
-						<td style="border-bottom: 1px solid black">
-							{{ invoice?.total_amount }}
-						</td>
-					</tr>
-				</tbody>
-			</table>
 		</div>
 	</div>
 </template>
--- app/pages/hotels/invoices/[id]/index.vue
+++ app/pages/hotels/invoices/[id]/index.vue
@@ -1,14 +1,36 @@
 <template>
 	<div class="">
 		<InvoicesPrint80mm />
-
+		<div id="invoice-image-container" class="mt-8 rounded-xl overflow-hidden">
+			<!--  -->
+		</div>
 		<div class="mt-8">
 			<InvoicesPage />
 		</div>
 	</div>
 </template>
 <script setup lang="ts">
+	import html2canvas from "html2canvas-pro"
 	definePageMeta({
 		layout: "main-layout",
+	})
+	onMounted(() => {
+		const invoicePage = document.getElementById("invoice-a4")
+		if (invoicePage) {
+			html2canvas(invoicePage).then((canvas) => {
+				const invoiceImageContainer = document.getElementById(
+					"invoice-image-container",
+				)
+				console.log(invoiceImageContainer)
+				if (invoiceImageContainer) {
+					canvas.style.width = "100%"
+					canvas.style.maxWidth = "210mm"
+					canvas.style.height = "auto"
+					canvas.style.margin = "auto"
+
+					invoiceImageContainer.appendChild(canvas)
+				}
+			})
+		}
 	})
 </script>
