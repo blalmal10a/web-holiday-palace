044c1d924d05bbfe8cc3726fbdd901ff302326d6
blalmal10a <malsawma7777@gmail.com>
2026-02-08 23:56:32 +0530

    feat: update booking calendar menu for dorm page

--- app/pages/hotels/dorm.vue
+++ app/pages/hotels/dorm.vue
@@ -103,18 +103,23 @@ onBeforeUnmount(
 								"
 							>
 								<template v-if="cell.bookingInfo">
-									<div class="p-4 border border-default ">
-										<div class="-mr-10 ml-4">
-											<div
-												class="rounded-lg px-2 whitespace-nowrap flex items-center h-8 relative cursor-pointer"
-												:class="{
-													'text-primary bg-elevated': !!cell.bookingInfo,
-												}"
-											>
-												{{ cell.bookingInfo?.client?.name }}
+									<BookingsCalendarMenu
+										v-if="cell.bookingInfo"
+										:cell="cell"
+									>
+										<div class="p-4 border border-default ">
+											<div class="-mr-10 ml-4">
+												<div
+													class="rounded-lg px-2 whitespace-nowrap flex items-center h-8 relative cursor-pointer"
+													:class="{
+														'text-primary bg-elevated': !!cell.bookingInfo,
+													}"
+												>
+													{{ cell.bookingInfo?.client?.name }}
+												</div>
 											</div>
 										</div>
-									</div>
+									</BookingsCalendarMenu>
 								</template>
 
 								<div
